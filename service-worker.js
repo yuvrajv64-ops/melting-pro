cc737aa7bb0b9b8b4ec170c37fadd2e91b715dfd
yuvrajv64-ops <yuvrajv64@gmail.com>
2025-11-27 01:55:38 +0530

    Remove unused event listeners from service worker
    Removed unused event listeners for 'activate' and 'fetch'.
    
    Signed-off-by: yuvrajv64-ops <yuvrajv64@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -16,15 +16,3 @@ self.addEventListener("install", (event) => {
   );
   self.skipWaiting();
 });
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request);
-    })
-  );
-});
